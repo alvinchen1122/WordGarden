82583ba8e98814c1b637b005e881841f4c1d56bb
Alvin Chen <chenbog@bc.edu>
2020-09-11 20:51:15 -0400

    Initial Commit

---
+++ WordGarden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by Alvin Chen on 9/11/20.
+//  Copyright © 2020 Alvin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
